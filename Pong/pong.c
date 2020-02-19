f4735d5dc76d188136588e40aa55895c2814aa5c
alejandro <alejandroaguilarvea@gmail.com>
2020-02-19 15:47:07 -0800

    PING PONG

--- Pong/pong.c
+++ Pong/pong.c
@@ -15,7 +15,6 @@ typedef struct game_element
 
 void gotoxy(char x, char y);
 void print_bar(game_element *bar,char cursor);
-void print_bar_npc(game_element *bar);
 void print_ball(game_element *ball_points);
 void pong_cal(game_element *ball_set,game_element *bar);
 void hidecursor();
@@ -24,18 +23,15 @@ COORD coord={0,0};
 
 int main()
 {
-game_element ball,bar,npc_bar;
+game_element ball,bar;
 clock_t start, stop;
 //////////////////////////////////////////////////////
 char entrada;
-int count_ball=0, count_bar=0,flag_speed=0;
+int count_ball=0, count_bar=0 , flag_speed=0;
 ////////////////////////////////////
 bar.coord_x=0;
 bar.coord_y=24;
 bar.time=20;
-npc_bar.coord_x=0;
-npc_bar.coord_y=0;
-npc_bar.time=20;
 ball.coord_x=0;
 ball.coord_y=0;
 ball.time=20;
@@ -73,7 +69,6 @@ while(1)
 
         case '0':
             ball.time=20;
-            npc_bar.time=20;
             bar.time=20;
             flag_speed=0;
 
@@ -92,7 +87,6 @@ while(1)
 
         case '2':
             ball.time=20;
-            npc_bar.time=20;
             bar.time=20;
             flag_speed=1;
 
@@ -112,13 +106,11 @@ while(1)
             system("cls");
             gotoxy(bar.coord_x,bar.coord_y);
             printf("%c%c%c%c%c%c",219,219,219,219,219,219);
+            }
         }
-
-        }
-
         stop=clock();
 
-        if((stop-start)>=5)
+        if((stop-start)>=1)
         {
             if((count_ball)>ball.time)
         {
@@ -126,24 +118,11 @@ while(1)
             count_ball=0;
         }
 
-         if((count_bar)>npc_bar.time)
-       {
-
-            print_bar_npc(&ball);
-            count_bar=0;
-       }
             count_ball++;
             count_bar++;
             start=clock();
         }
 }
-
-
-
-
-
-
-
 return 0;
 }
 
@@ -160,128 +139,93 @@ void print_bar(game_element *bar,char cursor)
 
 if(bar->coord_x >=1 && cursor=='a')
     {
-    bar->coord_x--;
-    gotoxy((bar->coord_x)+1,bar->coord_y);
+
+    gotoxy((bar->coord_x),bar->coord_y);
     printf("%c%c%c%c%c%c",32,32,32,32,32,32);
+    bar->coord_x--;
     gotoxy(bar->coord_x,bar->coord_y);
     printf("%c%c%c%c%c%c",219,219,219,219,219,219);
     }
 if(bar->coord_x <=78 && cursor=='d')
     {
-    bar->coord_x++;
-    gotoxy(bar->coord_x-1,bar->coord_y);
-    printf("%c%c%c%c%c%c",32,32,32,32,32,32);
     gotoxy(bar->coord_x,bar->coord_y);
-    printf("%c%c%c%c%c%c",219,219,219,219,219,219);
-    }
-}
-
-void print_bar_npc(game_element *bar)
-{
-
-if(bar->coord_x >=1)
-    {
-    gotoxy((bar->coord_x)+2,0);
     printf("%c%c%c%c%c%c",32,32,32,32,32,32);
-    gotoxy(bar->coord_x,0);
-    printf("%c%c%c%c%c%c",219,219,219,219,219,219);
-    }
-if(bar->coord_x <=78)
-    {
-    gotoxy((bar->coord_x)-2 ,0);
-    printf("%c%c%c%c%c%c",32,32,32,32,32,32);
-    gotoxy(bar->coord_x,0);
+    bar->coord_x++;
+    gotoxy(bar->coord_x,bar->coord_y);
     printf("%c%c%c%c%c%c",219,219,219,219,219,219);
     }
 }
 
 void print_ball(game_element *ball_points)
 {
+       gotoxy(ball_points->coord_x,ball_points->coord_y);
+       printf("%c",32);
+       gotoxy(ball_points->coord_x,0);
+       printf("%c%c%c%c%c%c",32,32,32,32,32,32);
+       //////////////////////////////////////////////////
        ball_points->coord_x+=ball_points->direct_x;
        ball_points->coord_y+=ball_points->direct_y;
        gotoxy(ball_points->coord_x,ball_points->coord_y);
        printf("%c",254);
+       gotoxy(ball_points->coord_x,0);
+       printf("%c%c%c%c%c%c",219,219,219,219,219,219);
 }
 
 void pong_cal(game_element *ball,game_element *bar)
 {
-
-    if((ball->coord_x == bar->coord_x) && ball->coord_y == bar->coord_y)
-{
+if((ball->coord_x == bar->coord_x) && ball->coord_y == bar->coord_y)
+    {
     ball->direct_y=-1;
-}
-
+    }
 if((ball->coord_x == bar->coord_x+3) && ball->coord_y == bar->coord_y)
-{
-
+    {
     ball->direct_y=-1;
-}
-
+    }
 if((ball->coord_x == bar->coord_x-3) && ball->coord_y == bar->coord_y)
-{
+    {
    ball->direct_y=-1;
-}
-
+    }
 if((ball->coord_x == bar->coord_x+2) && ball->coord_y == bar->coord_y)
-{
-
+    {
     ball->direct_y=-1;
-}
-
+    }
 if((ball->coord_x == bar->coord_x-2) && ball->coord_y == bar->coord_y)
-{
-   ball->direct_y=-1;
-}
-
-if((ball->coord_x == bar->coord_x+1) && ball->coord_y == bar->coord_y)
-{
-
+    {
     ball->direct_y=-1;
-}
-
+    }
+if((ball->coord_x == bar->coord_x+1) && ball->coord_y == bar->coord_y)
+    {
+    ball->direct_y=-1;
+    }
 if((ball->coord_x == bar->coord_x-1) && ball->coord_y == bar->coord_y)
-{
-   ball->direct_y=-1;
-}
-
+    {
+    ball->direct_y=-1;
+    }
 if((ball->coord_x == bar->coord_x+4) && ball->coord_y == bar->coord_y)
-{
-   ball->direct_y=-1;
-}
-
+    {
+     ball->direct_y=-1;
+    }
 if((ball->coord_x == bar->coord_x+5) && ball->coord_y == bar->coord_y)
-{
-   ball->direct_y=-1;
-}
-
-
-
+    {
+    ball->direct_y=-1;
+    }
 if(ball->coord_x == 80)
    {
-      ball->direct_x=-1;
+    ball->direct_x=-1;
    }
-
 if(ball->coord_x==0)
    {
-       ball->direct_x=1;
+    ball->direct_x=1;
    }
-
-
 if(ball->coord_y == 0)
-    {
-        ball->direct_y=1;
+   {
+    ball->direct_y=1;
+   }
+if(ball->coord_y == 25)
+{
+    printf("Perdiste el juego");
+    getch();
     }
-
-if(ball->coord_y==25)
-    {
-    ball->direct_y=-1;
-    system("cls");
-    }
-
-//if(ball->coord_y == 25)
-  //  {
-  //  printf("Perdiste el juego");
-    //}
 }
 
 void hidecursor()
